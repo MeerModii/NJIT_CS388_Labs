f5d82810bab603a313892c4fb2454ecfa38bfe12
Meer Modi <116285279+MeerModii@users.noreply.github.com>
2026-02-05 13:22:19 -0500

    Lab2

---
+++ Lab_2/app/src/main/java/com/example/lab_2/Email.kt
@@ -0,0 +1,7 @@
+package com.example.lab_2
+
+class Email(
+    val sender: String,
+    val title: String,
+    val summary: String) {
+}
---
+++ Lab_2/app/src/main/java/com/example/lab_2/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.lab_2
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var emails: MutableList<Email>
+    lateinit var adapter: EmailAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Lookup the RecyclerView in activity layout
+        val emailsRv = findViewById<RecyclerView>(R.id.emailsRv)
+        // Fetch the list of emails
+        emails = EmailFetcher.getEmails()
+        // Create adapter passing in the list of emails
+        adapter = EmailAdapter(emails)
+        // Attach the adapter to the RecyclerView to populate items
+        emailsRv.adapter = adapter
+        // Set layout manager to position the items
+        emailsRv.layoutManager = LinearLayoutManager(this)
+
+        findViewById<Button>(R.id.loadMoreBtn).setOnClickListener {
+            // Fetch next 5 emails
+            val newEmails = EmailFetcher.getNext5Emails()
+            // Add the new emails to our existing list of emails
+            emails.addAll(newEmails)
+            // Notify the adapter that the dataset has changed
+            adapter.notifyDataSetChanged()
+        }
+    }
+}
