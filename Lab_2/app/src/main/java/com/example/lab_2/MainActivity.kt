fb89afbe9cdbcb22994c4099c85dba679690b7b6
Meer Modi <116285279+MeerModii@users.noreply.github.com>
2026-02-05 13:27:46 -0500

    update

--- Lab_2/app/src/main/java/com/example/lab_2/MainActivity.kt
+++ Lab_2/app/src/main/java/com/example/lab_2/MainActivity.kt
@@ -15,23 +15,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Lookup the RecyclerView in activity layout
         val emailsRv = findViewById<RecyclerView>(R.id.emailsRv)
-        // Fetch the list of emails
         emails = EmailFetcher.getEmails()
-        // Create adapter passing in the list of emails
         adapter = EmailAdapter(emails)
-        // Attach the adapter to the RecyclerView to populate items
         emailsRv.adapter = adapter
-        // Set layout manager to position the items
         emailsRv.layoutManager = LinearLayoutManager(this)
 
         findViewById<Button>(R.id.loadMoreBtn).setOnClickListener {
-            // Fetch next 5 emails
             val newEmails = EmailFetcher.getNext5Emails()
-            // Add the new emails to our existing list of emails
             emails.addAll(newEmails)
-            // Notify the adapter that the dataset has changed
             adapter.notifyDataSetChanged()
         }
     }
